079d51104390507c4a109e6839c17e32ebbc4aa5
anna-valchuk <annaeme@gmail.com>
2022-11-07 12:08:53 +0100

    quest3 done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -15,11 +16,12 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
 
-const usersHandlers = require("./usersHandlers"); 
+const usersHandlers = require("./usersHandlers");
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-
+app.post("/api/users", usersHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
--- usersHandlers.js
+++ usersHandlers.js
@@ -1,23 +1,22 @@
 const database = require("./database");
 
-const getUsers = (req,res) => {
-database
-  .query("select * from users")
-  .then(([users]) => {
-   res.json(users);
-  })
- 
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error retrieving data from database");
-  });
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
-
 const getUsersById = (req, res) => {
-    const id = parseInt(req.params.id);
-    
-    database
+  const id = parseInt(req.params.id);
+
+  database
     .query("select * from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
@@ -30,10 +29,26 @@ const getUsersById = (req, res) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
     });
-  };
+};
 
-  module.exports = {
-    getUsers,
-    getUsersById,
-  };
-  
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUsersById,
+  postUsers,
+};
