188c27accc6156aa7cf47a40fb8f9c5b98d832e4
anna-valchuk <annaeme@gmail.com>
2022-11-08 09:22:06 +0100

    quest4 done

--- usersHandlers.js
+++ usersHandlers.js
@@ -43,12 +43,34 @@ const postUsers = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error saving the movie");
+      res.status(500).send("Error saving the user");
     });
 };
 
+const updateUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "UPDATE users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
 module.exports = {
   getUsers,
   getUsersById,
   postUsers,
+  updateUsers,
 };
